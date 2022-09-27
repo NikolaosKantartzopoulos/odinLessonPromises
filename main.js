cea1ca1fe69f2b09a3fcd5c73ffde2dd3846528e
Nikolaos Kantartzopoulos <95440122+NikolaosKantartzopoulos@users.noreply.github.com>
2022-09-27 14:28:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 let t1= document.getElementById("testH1");
 t1.style.backgroundColor = "red";
 let tr = true;
+let sr = false;
 
 
 let p1 = new Promise ((resolve,reject)=>{ 
   t1.style.backgroundColor = 'limegreen';
   tr
-  ? resolve('123')
+  ? resolve('1')
   : reject('Rejected')
 }).then(
     resolve => {
-      t1.textContent = resolve; 
+      t1.textContent = `0 ${resolve}`; 
       setTimeout(()=>{
-	console.log('ready to go violet');
-	t1.style.backgroundColor = 'violet'
+	  t1.textContent = `1200 ready to go violet`;
+	  t1.style.backgroundColor = 'violet';
 	}, 1200);
       return 'yellow';
     }, 
@@ -21,8 +22,16 @@ let p1 = new Promise ((resolve,reject)=>{
   .then((arg1)=>{
       setTimeout(()=>{
         t1.style.backgroundColor = arg1;
-        console.log(`Last color ${arg1}`)
+        t1.textContent = `3000 Last color ${arg1}`;
       },3000);
+      return 'orange';
     }
     , reject=>{console.log('last reject')}
-)	
+  ).then((sirLast)=>{
+      setTimeout(()=>{
+        t1.style.backgroundColor = sirLast;
+        t1.textContent = `5000 Seriously last color -> ${sirLast}`
+      },5000);
+
+  }
+  )	
